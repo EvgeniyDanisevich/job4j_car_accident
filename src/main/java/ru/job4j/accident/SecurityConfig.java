8961078ef88f33a9e1f4b46e53d0f0a1160e8f7a
EvgeniyDanisevich <a937va@gmail.com>
2021-06-27 20:14:42 +0300

    1. Авторизация через JDBC [#2094]

--- src/main/java/ru/job4j/accident/SecurityConfig.java
+++ src/main/java/ru/job4j/accident/SecurityConfig.java
@@ -10,26 +10,34 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    @Autowired
+    DataSource ds;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder)
-                .withUser("user").password(passwordEncoder.encode("123456")).roles("USER")
-                .and()
-                .withUser("admin").password(passwordEncoder.encode("123456")).roles("USER", "ADMIN");
+//        auth.jdbcAuthentication()
+//                .dataSource(ds)
+//                .passwordEncoder(passwordEncoder)
+//                .withUser("admin")
+//                .password(passwordEncoder.encode("123456"))
+//                .roles("USER", "ADMIN");
+        auth.jdbcAuthentication()
+                .dataSource(ds);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -52,5 +60,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .disable();
     }
-
 }
